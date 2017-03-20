8c5f8b969f7c8f897ad13fa279a0ff29337305f0
Blake Womack <lakebayomackway@gmail.com>
2017-03-20 12:49:11 -0700

    add story.vtl, mod layout and App.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,5 +16,16 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/story", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("person1", request.queryParams("person1"));
+      model.put("person2", request.queryParams("person2"));
+      model.put("animal", request.queryParams("animal"));
+      model.put("exclamation", request.queryParams("exclamation"));
+      model.put("verb", request.queryParams("verb"));
+      model.put("noun", request.queryParams("noun"));
+      model.put("template", "templates/story.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
